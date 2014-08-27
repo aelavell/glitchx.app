0917fada32652d82796ad9d46d5c24292d3ab922
Allan Lavell <alavell@gmail.com>
2014-08-27 13:39:40 -0300

    Delete yr hard drive

--- upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -f Archive.zip
-zip -r Archive.zip *
-scp Archive.zip moriarty@alexmoriarty.com:/home/content/48/10403148/html/sites/alavell/glitchwizard.com
-ssh moriarty@alexmoriarty.com 'cd /home/content/48/10403148/html/sites/alavell/glitchwizard.com;unzip -o Archive.zip;rm Archive.zip'
-rm Archive.zip
