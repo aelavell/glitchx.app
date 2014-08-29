ef7efbd276e2f0224660a8a3260eea134daa2440
Allan Lavell <alavell@gmail.com>
2014-08-29 16:59:31 -0300

    Fix upload script. Add mp4 for safari

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f Archive.zip
 zip -r Archive.zip *
-scp Archive.zip root@107.170.87.159:/usr/share/nginx/www/test/
-ssh root@107.170.87.159 'cd /usr/share/nginx/www/test/;unzip -o Archive.zip;rm Archive.zip'
+scp Archive.zip root@107.170.87.159:/usr/share/nginx/www/
+ssh root@107.170.87.159 'cd /usr/share/nginx/www/;unzip -o Archive.zip;rm Archive.zip'
 rm Archive.zip
